c09443b3c245e3890fafccc1228f7b9348aa3aa0
js.lozano@globant.com <142308Jslp>
2022-04-26 08:29:44 -0500

    base page setted

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    public Logger log = Logger.getLogger(BasePage.class);
+
+    public BasePage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, 15);
+        this.driver = driver;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait() {
+        return wait;
+    }
 }

bc0131a31fe393760a60a2f3ecab44d47974d342
js.lozano@globant.com <142308Jslp>
2022-04-25 23:42:08 -0500

    driver class completed

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,4 +1,29 @@
 package driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Driver {
+
+    private WebDriver driver;
+
+    public Driver(String browser){
+        switch (browser){
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
